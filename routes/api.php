fe98702f6061a3a27e2272320e36d1e286569c00
Muhammed Salama <32281997+MohammedSalama@users.noreply.github.com>
2023-06-01 22:32:56 +0300

    Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * AUTHENTICATION
+ */
+//Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+/**
+ * PROTECTED ROUTES
+ */
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+
+    /**
+     * Logout Function
+     */
+    Route::post('logout', [AuthController::class, 'logout']);
 });
