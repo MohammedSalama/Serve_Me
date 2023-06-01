dcf4b52177377e7879e1ac36dbe55809a80475a2
Muhammed Salama <32281997+MohammedSalama@users.noreply.github.com>
2023-06-01 23:08:44 +0300

    Route_Api

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,32 @@ Route::post('login', [AuthController::class, 'login']);
  */
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
+    /**
+     * Route Slider Api
+     */
+    Route::get('slider/{id}',[\App\Http\Controllers\Api\SliderController::class,'show']);
+    Route::patch('slider/{id}',[\App\Http\Controllers\Api\SliderController::class,'update']);
+    Route::delete('slider/{id}',[\App\Http\Controllers\Api\SliderController::class,'delete']);
+    Route::post('slider',[\App\Http\Controllers\Api\SliderController::class,'create']);
+    Route::get('/slider',[\App\Http\Controllers\Api\SliderController::class,'index']);
+
+    /**
+     * Route About Api
+     */
+    Route::get('about/{id}',[\App\Http\Controllers\Api\AboutController::class,'show']);
+    Route::patch('about/{id}',[\App\Http\Controllers\Api\AboutController::class,'update']);
+    Route::delete('about/{id}',[\App\Http\Controllers\Api\AboutController::class,'delete']);
+    Route::post('about',[\App\Http\Controllers\Api\AboutController::class,'create']);
+    Route::get('/about',[\App\Http\Controllers\Api\AboutController::class,'index']);
+
+    /**
+     * Route Screen Api
+     */
+    Route::get('screen/{id}',[\App\Http\Controllers\Api\ScreenController::class,'show']);
+    Route::patch('screen/{id}',[\App\Http\Controllers\Api\ScreenController::class,'update']);
+    Route::delete('screen/{id}',[\App\Http\Controllers\Api\ScreenController::class,'delete']);
+    Route::post('screen',[\App\Http\Controllers\Api\ScreenController::class,'create']);
+    Route::get('screen',[\App\Http\Controllers\Api\ScreenController::class,'index']);
 
     /**
      * Logout Function
